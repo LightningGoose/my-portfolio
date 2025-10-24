0f046362c24a62d547100ddaa3a73d19052a7075
LightningGoose <115466765+LightningGoose@users.noreply.github.com>
2025-10-24 11:23:41 +0200

    Update frontend dependencies and docs build output
    Added new build artifacts to docs/assets and updated docs/index.html. Updated frontend dependencies in package.json and package-lock.json, and modified Vite config in vite.config.ts, likely to support new build output or features.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portifolio/',
+  base: '/my-portfolio/',
   build: { outDir: '../docs' }   // <-- build to repo/docs
 })
